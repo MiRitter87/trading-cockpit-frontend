83da695db280fdcb0807bf8f54b53619ef94bc35
Michael <michaelritter@gmx.net>
2022-04-05 08:42:19 +0200

    Application skeleton created.

---
+++ webapp/Component.js
@@ -0,0 +1,22 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function (UIComponent) {
+  	"use strict";
+	return UIComponent.extend("trading-cockpit-frontend.Component", {
+
+		metadata : {
+         	manifest: "json"
+      	},	
+
+		/**
+		 * Initializes the component.
+		 */
+      	init : function () {
+        	//call the init function of the parent
+        	UIComponent.prototype.init.apply(this, arguments);
+
+			//create the views based on the url/hash
+           // this.getRouter().initialize();
+      	}
+   	});
+});
