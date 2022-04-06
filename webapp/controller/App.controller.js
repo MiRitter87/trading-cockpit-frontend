46871c83ff25885eac612c7470b6f329097449a7
Michael <michaelritter@gmx.net>
2022-04-06 10:12:59 +0200

    Define navigation menu item for price alert and handle item click.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,6 +4,11 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.App", {
-		
+		/**
+		 * Handles the click at the price alert entry in the menu.
+		 */
+		onPriceAlertPressed : function() {
+			alert("Price Alert pressed");
+		}
 	});
 });
