47c2c4252fb3714f7215f616b3f9f74bd20e31ee
Michael <michaelritter@gmx.net>
2022-04-07 08:25:54 +0200

    Basic view and controller for price alert creation added.

--- webapp/controller/priceAlert/PriceAlertMenu.controller.js
+++ webapp/controller/priceAlert/PriceAlertMenu.controller.js
@@ -4,6 +4,12 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.priceAlert.PriceAlertMenu", {
-		
+		/**
+		 * Handles click at the price alert create tile.
+		 */
+		onPriceAlertCreatePressed : function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("priceAlertCreateRoute");	
+		},
 	});
 });
