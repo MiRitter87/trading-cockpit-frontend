ab88f5915503564b82bd1cfba3f67cf819b4e78e
Michael <michaelritter@gmx.net>
2022-04-20 08:16:05 +0200

    Load price alert data on navigation to display view.

--- webapp/controller/priceAlert/PriceAlertDisplay.controller.js
+++ webapp/controller/priceAlert/PriceAlertDisplay.controller.js
@@ -1,7 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"../../model/formatter"
-], function (Controller, formatter) {
+	"./PriceAlertController",
+	"../../model/formatter",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"
+], function (Controller, PriceAlertController, formatter, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.priceAlert.PriceAlertDisplay", {
@@ -12,7 +15,43 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("priceAlertDisplayRoute").attachMatched(this._onRouteMatched, this);
+		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function () {
+			//Query master data every time a user navigates to this view. This assures that changes are being displayed in the ComboBox.
+			PriceAlertController.queryPriceAlertsByWebService(this.queryPriceAlertsCallback, this, true);
 			
+			this.getView().setModel(null, "selectedPriceAlert");
+			//this.resetUIElements();
+    	},
+
+
+		/**
+		 * Callback function of the queryPriceAlerts RESTful WebService call in the PriceAlertController.
+		 */
+		queryPriceAlertsCallback : function(oReturnData, oCallingController, bShowSuccessMessage) {
+			var oModel = new JSONModel();
+			var oResourceBundle = oCallingController.getOwnerComponent().getModel("i18n").getResourceBundle();
+			
+			if(oReturnData.data != null) {
+				oModel.setData(oReturnData.data);
+				//PriceAlertController.initializeDatesAsObject(oModel.oData.priceAlert);
+				
+				if(bShowSuccessMessage == true)
+					MessageToast.show(oResourceBundle.getText("priceAlertDisplay.dataLoaded"));			
+			}
+			
+			if(oReturnData.data == null && oReturnData.message != null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}                                                               
+			
+			oCallingController.getView().setModel(oModel, "priceAlerts");
 		}
 	});
 });
