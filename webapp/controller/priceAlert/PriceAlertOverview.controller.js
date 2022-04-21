238a15e27c02a8eba1bd8064c2579bb6859a8c85
Michael <michaelritter@gmx.net>
2022-04-21 09:11:37 +0200

    Call WebService to load price alerts for overview.

--- webapp/controller/priceAlert/PriceAlertOverview.controller.js
+++ webapp/controller/priceAlert/PriceAlertOverview.controller.js
@@ -1,6 +1,9 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"./PriceAlertController",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"
+], function (Controller, PriceAlertController, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.priceAlert.PriceAlertOverview", {
@@ -8,7 +11,39 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("priceAlertOverviewRoute").attachMatched(this._onRouteMatched, this);
+		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function () {
+			//Query master data every time a user navigates to this view. This assures that changes are being displayed in the ComboBox.
+			PriceAlertController.queryPriceAlertsByWebService(this.queryPriceAlertsCallback, this, true);
+    	},
+
+
+		/**
+		 * Callback function of the queryPriceAlert RESTful WebService call in the PriceAlertController.
+		 */
+		queryPriceAlertsCallback : function(oReturnData, oCallingController, bShowSuccessMessage) {
+			var oModel = new JSONModel();
+			var oResourceBundle = oCallingController.getOwnerComponent().getModel("i18n").getResourceBundle();
 			
+			if(oReturnData.data != null) {
+				oModel.setData(oReturnData.data);
+				
+				if(bShowSuccessMessage == true)
+					MessageToast.show(oResourceBundle.getText("priceAlertOverview.dataLoaded"));			
+			}
+			
+			if(oReturnData.data == null && oReturnData.message != null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}                                                               
+			
+			oCallingController.getView().setModel(oModel, "priceAlerts");
 		}
 	});
 });
