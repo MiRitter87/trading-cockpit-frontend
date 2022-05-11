602b4302924e541ecc9177951c770db6f9c6828f
Michael <michaelritter@gmx.net>
2022-05-11 10:37:16 +0200

    Initialize IntervalTrigger in price alert feed.

--- webapp/controller/priceAlert/PriceAlertFeed.controller.js
+++ webapp/controller/priceAlert/PriceAlertFeed.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/IntervalTrigger"
+], function (Controller, IntervalTrigger) {
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.priceAlert.PriceAlertFeed", {		
@@ -8,7 +9,16 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
-			
+			var intervalTrigger = new IntervalTrigger(10000);
+			intervalTrigger.addListener(this.getAlerts);
+		},
+		
+		
+		/**
+		 * Gets the price alerts.
+		 */
+		getAlerts: function () {
+			alert(Date.now());
 		}
 	});
 });
