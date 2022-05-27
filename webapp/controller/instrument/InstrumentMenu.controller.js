25912ca0670fced0a6f6214bd2fe6dea736453e4
Michael <michaelritter@gmx.net>
2022-05-27 08:54:52 +0200

    Add tiles to instrument menu.

--- webapp/controller/instrument/InstrumentMenu.controller.js
+++ webapp/controller/instrument/InstrumentMenu.controller.js
@@ -4,6 +4,39 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.instrument.InstrumentMenu", {
-
+		/**
+		 * Handles click at the instrument create tile.
+		 */
+		onPriceAlertCreatePressed : function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			//oRouter.navTo("instrumentCreateRoute");	
+		},
+		
+		
+		/**
+		 * Handles click at the instrument edit tile.
+		 */
+		onPriceAlertEditPressed : function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			//oRouter.navTo("instrumentEditRoute");	
+		},
+		
+		
+		/**
+		 * Handles click at the instrument display tile.
+		 */
+		onPriceAlertDisplayPressed : function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			//oRouter.navTo("instrumentDisplayRoute");	
+		},
+		
+		
+		/**
+		 * Handles click at the instrument overview tile.
+		 */
+		onPriceAlertOverviewPressed : function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			//oRouter.navTo("instrumentOverviewRoute");	
+		},
 	});
 });
