165bdc9ede160795a08968cdb6616fa0b364d357
Michael <michaelritter@gmx.net>
2022-05-30 08:07:32 +0200

    Initialize model on navigation to instrument creation view.

--- webapp/controller/instrument/InstrumentCreate.controller.js
+++ webapp/controller/instrument/InstrumentCreate.controller.js
@@ -9,11 +9,36 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			
+			//Register an event handler that gets called every time the router navigates to this view.
+			oRouter.getRoute("instrumentCreateRoute").attachMatched(this._onRouteMatched, this);
+			
 			InstrumentController.initializeStockExchangeComboBox(this.getView().byId("stockExchangeComboBox"), 
 				this.getOwnerComponent().getModel("i18n").getResourceBundle());
 				
 			InstrumentController.initializeTypeComboBox(this.getView().byId("typeComboBox"), 
 				this.getOwnerComponent().getModel("i18n").getResourceBundle());
+		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function () {
+			//this.resetUIElements();
+			this.initializeInstrumentModel();
+    	},
+
+
+		/**
+		 * Initializes the instrument model to which the UI controls are bound.
+		 */
+		initializeInstrumentModel : function () {
+			var oInstrumentModel = new JSONModel();
+			
+			oInstrumentModel.loadData("model/instrument/instrumentCreate.json");
+			this.getView().setModel(oInstrumentModel, "newInstrument");
 		}
 	});
 });
