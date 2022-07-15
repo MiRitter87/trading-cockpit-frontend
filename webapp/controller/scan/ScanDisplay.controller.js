0c826834e4122f24a5f4c75044e0f31fe736c4c0
Michael <michaelritter@gmx.net>
2022-07-15 08:23:24 +0200

    Basic view and controller for scan display added.

---
+++ webapp/controller/scan/ScanDisplay.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("trading-cockpit-frontend.controller.scan.ScanDisplay", {
+		/**
+		 * Initializes the controller.
+		 */
+		onInit : function () {
+			
+		}
+	});
+});
