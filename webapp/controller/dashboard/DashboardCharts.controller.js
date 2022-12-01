cc67350925f810eb2dc10688b5bb639485736de4
MiRitter87 <michaelritter@gmx.net>
2022-12-01 10:41:29 +0100

    Define UI elements of DashboardCharts view and init type ComboBox.

--- webapp/controller/dashboard/DashboardCharts.controller.js
+++ webapp/controller/dashboard/DashboardCharts.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"../MainController",
+	"../Constants"
+], function (Controller, MainController, Constants) {
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.dashboard.DashboardCharts", {
@@ -8,7 +10,27 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
+			this.initializeTypeComboBox();
+		},
+		
+		
+		/**
+    	 * Handles the button press event of the refresh chart button.
+    	 */
+    	onRefreshPressed : function() {
+			//http://127.0.0.1:8080/trading-cockpit-backend/services/rest/statistics/chart?chartType=ADVANCE_DECLINE_NUMBER
+		},
+		
+		
+		/**
+		 * Initializes the ComboBox of chart type.
+		 */
+		initializeTypeComboBox : function () {
+			var oComboBox = this.getView().byId("typeComboBox");
+			var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
 			
+			MainController.addItemToComboBox(oComboBox, oResourceBundle, 
+				Constants.CHART_TYPE.ADVANCE_DECLINE_NUMBER, "dashboardCharts.type.advanceDeclineNumber");
 		}
 	});
 });
