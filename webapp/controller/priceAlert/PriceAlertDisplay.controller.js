29d007618c7a0dcb5cb4e7bd3df1fd4316dd1eb8
MiRitter87 <michaelritter@gmx.net>
2023-06-06 08:23:03 +0200

    Add mail-related attributes to PriceAlertDisplay view.

--- webapp/controller/priceAlert/PriceAlertDisplay.controller.js
+++ webapp/controller/priceAlert/PriceAlertDisplay.controller.js
@@ -98,6 +98,10 @@ sap.ui.define([
 			this.getView().byId("triggerDistancePercentText").setText("");
 			this.getView().byId("triggerTimeText").setText("");
 			this.getView().byId("confirmationTimeText").setText("");
+			
+			this.getView().byId("sendMailCheckBox").setSelected(false);
+			this.getView().byId("alertMailAddressText").setText("");
+			this.getView().byId("mailTransmissionTimeText").setText("");
 		}
 	});
 });
