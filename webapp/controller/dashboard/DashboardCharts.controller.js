1a733ef52230c3dd0103dd5d7b5f8409aa3fd3d2
MiRitter87 <michaelritter@gmx.net>
2023-07-10 08:46:34 +0200

    Zoom time scale of TradingView chart and show crosshair.

--- webapp/controller/dashboard/DashboardCharts.controller.js
+++ webapp/controller/dashboard/DashboardCharts.controller.js
@@ -236,7 +236,7 @@ sap.ui.define([
 		onTradingViewPopupOpened : function (oCallingController) {
 			var divId = oCallingController.createId("chartContainer")
 			
-			//Remove previously created chart for subsequent chart creations.
+			//Remove previously created chart for subsequent chart creations
 			document.getElementById(divId).innerHTML = "";
 			
 			const chart = LightweightCharts.createChart(document.getElementById(divId), {
@@ -257,6 +257,18 @@ sap.ui.define([
 				    { time: '2019-04-19', value: 81.89 },
 				    { time: '2019-04-20', value: 74.43 },
 			]);
+			
+			//Automatically zoom the time scale to display all datasets over the full width of the chart.
+			chart.timeScale().fitContent();
+			
+			// Customizing the Crosshair
+			chart.applyOptions({
+    			crosshair: {
+			        // Change mode from default 'magnet' to 'normal'.
+			        // Allows the crosshair to move freely without snapping to datapoints
+			        mode: LightweightCharts.CrosshairMode.Normal
+    			},
+			});
 		},
 		
 		
