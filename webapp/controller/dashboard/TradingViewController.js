2decd7abd6ad36493393a5d7c48680bbd6a26468
MiRitter87 <michaelritter@gmx.net>
2023-07-11 08:51:06 +0200

    Get exact price within TradingView chart on click in chart panel.

--- webapp/controller/dashboard/TradingViewController.js
+++ webapp/controller/dashboard/TradingViewController.js
@@ -1,13 +1,14 @@
 sap.ui.define([
+	"sap/ui/model/json/JSONModel",
 	"./lightweight-charts.standalone.production"
-], function () {
+], function (JSONModel) {
 	"use strict";
 	return {
-		
 		/**
 		 * Handles initialization of the TradingView lightweight-charts component.
 		 */
 		openChart : function (oCallingController) {
+			var oChartModel = new JSONModel();
 			var divId = oCallingController.createId("chartContainer")
 			
 			//Remove previously created chart for subsequent chart creations
@@ -19,17 +20,17 @@ sap.ui.define([
 			});
 			
 			const lineSeries = chart.addLineSeries();
-				lineSeries.setData([
-				    { time: '2019-04-11', value: 80.01 },
-				    { time: '2019-04-12', value: 96.63 },
-				    { time: '2019-04-13', value: 76.64 },
-				    { time: '2019-04-14', value: 81.89 },
-				    { time: '2019-04-15', value: 74.43 },
-				    { time: '2019-04-16', value: 80.01 },
-				    { time: '2019-04-17', value: 96.63 },
-				    { time: '2019-04-18', value: 76.64 },
-				    { time: '2019-04-19', value: 81.89 },
-				    { time: '2019-04-20', value: 74.43 },
+			lineSeries.setData([
+			    { time: '2019-04-11', value: 80.01 },
+			    { time: '2019-04-12', value: 96.63 },
+			    { time: '2019-04-13', value: 76.64 },
+			    { time: '2019-04-14', value: 81.89 },
+			    { time: '2019-04-15', value: 74.43 },
+			    { time: '2019-04-16', value: 80.01 },
+			    { time: '2019-04-17', value: 96.63 },
+			    { time: '2019-04-18', value: 76.64 },
+			    { time: '2019-04-19', value: 81.89 },
+			    { time: '2019-04-20', value: 74.43 },
 			]);
 			
 			//Automatically zoom the time scale to display all datasets over the full width of the chart.
@@ -44,11 +45,12 @@ sap.ui.define([
     			},
 			});
 			
-			//Handle clicks in the chart.
-			chart.subscribeClick(this.onChartClicked);
+			//Handle clicks in the chart. The controller needs to be bound to access the data model within the handler.
+			chart.subscribeClick(this.onChartClicked.bind(oCallingController));
 			
-			//TODO Remove after test
-			console.log(lineSeries.coordinateToPrice(59));
+			//The lineSeries needs to be stored in the controller for further access later on.
+			oChartModel.setProperty("/lineSeries", lineSeries);
+			oCallingController.getView().setModel(oChartModel, "chartModel");
 		},
 		
 		
@@ -59,11 +61,13 @@ sap.ui.define([
 			if (!param.point) {
 		        return;
 		    }
-		    
-		    //TODO Get Series, apply lineSeries.coordinateToPrice(param.point.y)
-		    //console.log(lineSeries.coordinateToPrice(param.point.y));
 			
-		    console.log(`Click at ${param.point.x}, ${param.point.y}. The time is ${param.time}.`);
+		    var oChartModel = this.getView().getModel("chartModel");
+		    var lineSeries = oChartModel.getProperty("/lineSeries");
+		    
+		    //TODO Remove Test output
+		    console.log(`The price is ${lineSeries.coordinateToPrice(param.point.y)}`);
+			console.log(`The date is ${param.time}.`);
 		}
 	};
 });
