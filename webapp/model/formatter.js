d9b67b803ae93104ac9eba26178986e51ca8708e
MiRitter87 <michaelritter@gmx.net>
2023-10-04 09:28:57 +0200

    Use new formatter for date only UI elements.

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -5,6 +5,26 @@ sap.ui.define([
 	return {
 		/**
 		 * Formats the date provided by the backend WebService as milliseconds since 01/01/1970 to a human readable form.
+		 * The formatted result contains date and time.
+		 */
+		wsDateTimeFormatter : function (timestamp) {
+			var oDateFormat, oDate, sFormattedDate;
+			
+			if (typeof timestamp === 'undefined' || timestamp == null) {
+				return "";				
+			}
+			else {
+				oDateFormat = DateFormat.getDateInstance({pattern : "dd.MM.YYYY HH:mm:ss"});
+				oDate = new Date(parseInt(timestamp))
+				sFormattedDate = oDateFormat.format(oDate);
+				
+				return sFormattedDate;
+			}
+		},
+		
+		/**
+		 * Formats the date provided by the backend WebService as milliseconds since 01/01/1970 to a human readable form.
+		 * The formatted result contains only the date and no time component.
 		 */
 		wsDateFormatter : function (timestamp) {
 			var oDateFormat, oDate, sFormattedDate;
@@ -13,7 +33,7 @@ sap.ui.define([
 				return "";				
 			}
 			else {
-				oDateFormat = DateFormat.getDateInstance({pattern : "dd.MM.YYYY HH:mm:ss"});
+				oDateFormat = DateFormat.getDateInstance({pattern : "dd.MM.YYYY"});
 				oDate = new Date(parseInt(timestamp))
 				sFormattedDate = oDateFormat.format(oDate);
 				
