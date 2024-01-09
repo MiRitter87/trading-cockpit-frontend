b59417fbe9b95668ec75da408acfdff557ab1523
MiRitter87 <michaelritter@gmx.net>
2024-01-09 08:19:05 +0100

    Add A/D number tile to ChartMenu view.

--- webapp/controller/chart/ChartMenu.controller.js
+++ webapp/controller/chart/ChartMenu.controller.js
@@ -4,6 +4,12 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.chart.ChartMenu", {
-		
+		/**
+		 * Handles click at the Advance/Decline Number tile.
+		 */
+		onAdvanceDeclineNumberPressed : function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			//oRouter.navTo("dashboardStatisticRoute");	
+		},
 	});
 });
