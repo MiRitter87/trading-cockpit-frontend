f01d153b1598d324b0207351c2a47692931a2bfc
MiRitter87 <michaelritter@gmx.net>
2024-01-11 08:20:10 +0100

    Show chart information in quota above SMA(50) chart view.

--- webapp/controller/chart/ChartAboveSma50.controller.js
+++ webapp/controller/chart/ChartAboveSma50.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox"
+], function (Controller, MessageBox) {
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.chart.ChartAboveSma50", {
@@ -17,6 +18,20 @@ sap.ui.define([
 		 */
 		_onRouteMatched: function () {
 			
-    	}
+    	},
+    	
+    	
+    	/**
+    	 * Handles the button press event of the chart information button.
+    	 */
+    	onChartInformationPressed : function() {
+			var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			var mOptions = new Object();
+			var sTitle = oResourceBundle.getText("chartAboveSma50.info.title");
+			var sDescription = oResourceBundle.getText("chartAboveSma50.info.description");
+			
+			mOptions.title = sTitle;
+			MessageBox.information(sDescription, mOptions);
+		}
 	});
 });
