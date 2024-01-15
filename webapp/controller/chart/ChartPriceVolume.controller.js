2b215477118440c615073b0141cde46b7effc611
MiRitter87 <michaelritter@gmx.net>
2024-01-15 09:26:54 +0100

    Add UI elements to Price/Volume chart view.

--- webapp/controller/chart/ChartPriceVolume.controller.js
+++ webapp/controller/chart/ChartPriceVolume.controller.js
@@ -17,6 +17,70 @@ sap.ui.define([
 		 */
 		_onRouteMatched: function () {
 
-    	}
+    	},
+    	
+    	
+    	/**
+    	 * Handles the button press event of the chart information button.
+    	 */
+    	onChartInformationPressed : function() {
+	
+		},
+		
+		
+		/**
+		 * Handles the selection of an Instrument.
+		 */
+		onInstrumentSelectionChange : function (oControlEvent) {
+			
+		},
+		
+		
+		/**
+    	 * Handles the button press event of the refresh chart button.
+    	 */
+    	onRefreshPressed : function() {
+	
+		},
+		
+		
+		/**
+		 * Handles selection of the volume CheckBox.
+		 */
+		onVolumeCheckBoxSelect : function() {
+			
+		},
+		
+		
+		/**
+		 * Handles selection of an indicator for the price volume chart.
+		 */
+		onIndicatorSelectionChange : function(oControlEvent) {
+			
+		},
+		
+		
+		/**
+		 * Handles the button press event of the add object button.
+		 */
+		onAddObjectPressed : function() {
+		
+		},
+		
+		
+		/**
+		 * Handles the button press event of the edit object button.
+		 */
+		onEditObjectPressed : function() {
+			
+		},
+		
+		
+		/**
+		 * Handles the button press event of the overview object button.
+		 */
+		onObjectOverviewPressed : function() {
+			
+		}
 	});
 });
