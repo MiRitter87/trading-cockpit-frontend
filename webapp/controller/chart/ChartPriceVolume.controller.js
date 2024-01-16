39ff0fb7485ea9813ddf5751c4b29f9932d575ca
MiRitter87 <michaelritter@gmx.net>
2024-01-16 09:05:49 +0100

    Query instruments on navigation to Price/Volume chart view.

--- webapp/controller/chart/ChartPriceVolume.controller.js
+++ webapp/controller/chart/ChartPriceVolume.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"../MainController",
+	"../scan/ScanController",
 	"../Constants",
+	"sap/ui/model/json/JSONModel",
 	"sap/m/MessageBox"
-], function (Controller, MainController, Constants, MessageBox) {
+], function (Controller, MainController, ScanController, Constants, JSONModel, MessageBox) {
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.chart.ChartPriceVolume", {
@@ -22,6 +24,9 @@ sap.ui.define([
 		 * Handles the routeMatched-event when the router navigates to this view.
 		 */
 		_onRouteMatched: function () {
+			//Query only instruments that have quotations referenced. Otherwise no chart could be created.
+			ScanController.queryQuotationsByWebService(this.queryQuotationsCallback, this, false, Constants.SCAN_TEMPLATE.ALL);
+			
 			this.resetUIElements();
     	},
     	
@@ -93,6 +98,25 @@ sap.ui.define([
 		 */
 		onObjectOverviewPressed : function() {
 			
+		},
+		
+		
+		/**
+		 * Callback function of the queryQuotationsByWebService RESTful WebService call in the ScanController.
+		 */
+		queryQuotationsCallback : function(oReturnData, oCallingController) {
+			var oModel = new JSONModel();
+			
+			if(oReturnData.data != null) {
+				oModel.setSizeLimit(300);
+				oModel.setData(oReturnData.data);
+			}
+			
+			if(oReturnData.data == null && oReturnData.message != null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}
+			
+			oCallingController.getView().setModel(oModel, "quotations");
 		},
 		
 		
