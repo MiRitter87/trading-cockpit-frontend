42d4d66165ff9d3ff72c8ea8fcdd507e481a22c0
MiRitter87 <michaelritter@gmx.net>
2024-03-22 08:58:50 +0100

    Query instrument data on navigation to DashboardHealthStatus view.

--- webapp/controller/MainController.js
+++ webapp/controller/MainController.js
@@ -2,7 +2,9 @@ sap.ui.define([
 	"sap/ui/core/Fragment",
 	"sap/ui/core/Item",
 	"./Constants",
-], function (Fragment, Item, Constants) {
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Fragment, Item, Constants, Filter, FilterOperator) {
 	"use strict";
 	
 	return {
@@ -109,6 +111,35 @@ sap.ui.define([
 		getServerAddress : function() {
 			var sServerAddress = window.location.origin;
 			return sServerAddress;
+		},
+		
+		
+		/**
+		 * Applies a Filter to the ComboBox for Instrument selection.
+		 */
+		applyFilterToInstrumentsComboBox : function (oComboxBox, aAllowedInstrumentTypes) {
+			var oBinding = oComboxBox.getBinding("items");
+			var aFilters = new Array();
+			var oFilterType, oFilterTotal;
+			
+			if(aAllowedInstrumentTypes == undefined || aAllowedInstrumentTypes.length == 0)
+				return;
+				
+			for(var i = 0; i < aAllowedInstrumentTypes.length; i++) {
+				oFilterType = new Filter("instrument/type", FilterOperator.EQ, aAllowedInstrumentTypes[i]);
+				aFilters.push(oFilterType);
+			}
+			
+			if(oBinding == undefined)
+				return;
+			
+			//Connect filters via logical "OR".
+			var oFilterTotal = new Filter({
+				filters: aFilters,
+    			and: false
+  			});
+			
+			oBinding.filter([oFilterTotal]);
 		}
 	};
 });
--- webapp/controller/dashboard/DashboardHealthStatus.controller.js
+++ webapp/controller/dashboard/DashboardHealthStatus.controller.js
@@ -1,6 +1,10 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"../MainController",
+	"../scan/ScanController",
+	"../Constants",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MainController, ScanController, Constants, JSONModel) {
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.dashboard.DashboardHealthStatus", {
@@ -8,7 +12,51 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("dashboardHealthStatusRoute").attachMatched(this._onRouteMatched, this);
+		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function () {
+			//Query only instruments that have quotations referenced. Otherwise no health status can be determined.
+			ScanController.queryQuotationsByWebService(this.queryQuotationsCallback, this, false, Constants.SCAN_TEMPLATE.ALL);
+			
+			this.resetUIElements();
+    	},
+    	
+    	
+    	/**
+		 * Callback function of the queryQuotationsByWebService RESTful WebService call in the ScanController.
+		 */
+		queryQuotationsCallback : function(oReturnData, oCallingController) {
+			var oModel = new JSONModel();
+			var oInstrumentComboBox = oCallingController.getView().byId("instrumentComboBox");
+			
+			if(oReturnData.data != null) {
+				oModel.setSizeLimit(300);
+				oModel.setData(oReturnData.data);
+			}
+			
+			if(oReturnData.data == null && oReturnData.message != null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}
+			
+			oCallingController.getView().setModel(oModel, "quotations");
+			MainController.applyFilterToInstrumentsComboBox(oInstrumentComboBox,
+					[Constants.INSTRUMENT_TYPE.SECTOR, Constants.INSTRUMENT_TYPE.INDUSTRY_GROUP]);
+		},
+    	
+    	
+    	/**
+		 * Resets the UI elements.
+		 */
+		resetUIElements : function () {
+			var oInstrumentComboBox = this.getView().byId("instrumentComboBox");
 
+			oInstrumentComboBox.setSelectedKey("");
 		}
 	});
 });
