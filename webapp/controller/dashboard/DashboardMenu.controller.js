fd61a11b43197ebb7f229539185f2bbc841c6553
MiRitter87 <michaelritter@gmx.net>
2024-03-22 08:26:20 +0100

    Add initial view and controller for dashboard health status.

--- webapp/controller/dashboard/DashboardMenu.controller.js
+++ webapp/controller/dashboard/DashboardMenu.controller.js
@@ -10,6 +10,15 @@ sap.ui.define([
 		onStatisticPressed : function () {
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("dashboardStatisticRoute");	
+		},
+		
+		
+		/**
+		 * Handles click at the health status tile.
+		 */
+		onHealthStatusPressed : function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("dashboardHealthStatusRoute");	
 		}
 	});
 });
