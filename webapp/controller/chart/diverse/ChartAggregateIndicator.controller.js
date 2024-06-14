1c46fd2d9f6ca16abb5e6db9d93aad86b3d6ef4d
MiRitter87 <michaelritter@gmx.net>
2024-06-14 09:32:08 +0200

    Handle loading of Aggregate Indicator chart.

--- webapp/controller/chart/diverse/ChartAggregateIndicator.controller.js
+++ webapp/controller/chart/diverse/ChartAggregateIndicator.controller.js
@@ -4,8 +4,9 @@ sap.ui.define([
 	"../../scan/ScanController",
 	"../../Constants",
 	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageBox",
 	"sap/m/MessageToast"
-], function (Controller, MainController, ScanController, Constants, JSONModel, MessageToast) {
+], function (Controller, MainController, ScanController, Constants, JSONModel, MessageBox, MessageToast) {
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.chart.diverse.ChartAggregateIndicator", {
@@ -24,7 +25,45 @@ sap.ui.define([
 		_onRouteMatched: function () {
 			//Query only instruments that have quotations referenced. Otherwise no Aggregate Indicator can be determined.
 			ScanController.queryQuotationsByWebService(this.queryQuotationsCallback, this, false, Constants.SCAN_TEMPLATE.ALL);
+			
+			this.resetUIElements();
     	},
+    	
+    	
+    	/**
+    	 * Handles the button press event of the refresh chart button.
+    	 */
+    	onRefreshPressed : function() {
+			var oImage = this.getView().byId("chartImage");
+			var bIsInputValid = this.isInputValid();
+			var sChartUrl;
+			
+			if(bIsInputValid) {
+				sChartUrl = this.getChartUrl();
+				oImage.setSrc(sChartUrl);
+			}
+			else {				
+				oImage.setSrc(null);
+			}
+		},
+		
+		
+		/**
+		 * Handles error during loading of the chart image using the given URL.
+		 */
+		onChartImageError : function() {
+			var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			var oImage = this.getView().byId("chartImage");
+			var sImageSrc = oImage.getProperty("src");
+			
+			if(sImageSrc == "")
+				return;		//There was no image to load.
+			
+			//The backend currently only supports a response with error code 404 and standard error page with response text.
+			//The response site would have to be parsed in order to get the message from the backend.
+			//Therefore only a generic error message is being displayed at the moment.
+			MessageToast.show(oResourceBundle.getText("chartAggregateIndicator.getChartError"));
+		},
     	
     	
     	/**
@@ -46,6 +85,55 @@ sap.ui.define([
 			oCallingController.getView().setModel(oModel, "quotations");
 			MainController.applyFilterToInstrumentsComboBox(oInstrumentComboBox, "instrument/type",
 					[Constants.INSTRUMENT_TYPE.SECTOR, Constants.INSTRUMENT_TYPE.INDUSTRY_GROUP]);
+		},
+		
+		
+		/**
+		 * Validates the user input. Prompts messages in input is not valid.
+		 */
+		isInputValid : function () {
+			var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			var oInstrumentComboBox = this.getView().byId("instrumentComboBox");
+			var sSelectedInstrumentId = oInstrumentComboBox.getSelectedKey();
+			
+			if(sSelectedInstrumentId == "") {
+				MessageBox.error(oResourceBundle.getText("chartAggregateIndicator.noInstrumentSelected"));
+				return false;
+			}
+			
+			return true;
+		},
+		
+		
+		/**
+		 * Determines the URL of the chart.
+		 */
+		getChartUrl : function() {
+			var oInstrumentComboBox = this.getView().byId("instrumentComboBox");
+			var sSelectedInstrumentId = oInstrumentComboBox.getSelectedKey();
+			var sServerAddress = MainController.getServerAddress();
+			var sWebServiceBaseUrl = this.getOwnerComponent().getModel("webServiceBaseUrls").getProperty("/chart");
+			var sChartUrl = sServerAddress + sWebServiceBaseUrl;
+			
+			sChartUrl = sChartUrl + "/aggregateIndicator/" + sSelectedInstrumentId;
+			
+			//The randomDate parameter is not evaluated by the backend. 
+			//It assures that the image is not loaded from the browser cache by generating a new query URL each time.
+			sChartUrl = sChartUrl  + "?randomDate=" + new Date().getTime();
+			
+			return sChartUrl;
+		},
+		
+		
+		/**
+		 * Resets the UI elements.
+		 */
+		resetUIElements : function () {
+			var oInstrumentComboBox = this.getView().byId("instrumentComboBox");
+			var oImage = this.getView().byId("chartImage");
+
+			oInstrumentComboBox.setSelectedKey("");
+			oImage.setSrc(null);
 		}
 	});
 });
