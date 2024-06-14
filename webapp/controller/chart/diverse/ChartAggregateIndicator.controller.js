447e4fd8c592acb37321ca94d54863a64d51b66d
MiRitter87 <michaelritter@gmx.net>
2024-06-14 09:01:37 +0200

    Initialize instruments on navigation to Aggregate Indicator chart view.

--- webapp/controller/chart/diverse/ChartAggregateIndicator.controller.js
+++ webapp/controller/chart/diverse/ChartAggregateIndicator.controller.js
@@ -1,6 +1,11 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"../../MainController",
+	"../../scan/ScanController",
+	"../../Constants",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"
+], function (Controller, MainController, ScanController, Constants, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.chart.diverse.ChartAggregateIndicator", {
@@ -8,7 +13,39 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("chartAggregateIndicatorRoute").attachMatched(this._onRouteMatched, this);
+		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function () {
+			//Query only instruments that have quotations referenced. Otherwise no Aggregate Indicator can be determined.
+			ScanController.queryQuotationsByWebService(this.queryQuotationsCallback, this, false, Constants.SCAN_TEMPLATE.ALL);
+    	},
+    	
+    	
+    	/**
+		 * Callback function of the queryQuotationsByWebService RESTful WebService call in the ScanController.
+		 */
+		queryQuotationsCallback : function(oReturnData, oCallingController) {
+			var oModel = new JSONModel();
+			var oInstrumentComboBox = oCallingController.getView().byId("instrumentComboBox");
 			
+			if(oReturnData.data != null) {
+				oModel.setSizeLimit(300);
+				oModel.setData(oReturnData.data);
+			}
+			
+			if(oReturnData.data == null && oReturnData.message != null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}
+			
+			oCallingController.getView().setModel(oModel, "quotations");
+			MainController.applyFilterToInstrumentsComboBox(oInstrumentComboBox, "instrument/type",
+					[Constants.INSTRUMENT_TYPE.SECTOR, Constants.INSTRUMENT_TYPE.INDUSTRY_GROUP]);
 		}
 	});
 });
