3569b7d7120e5b82b300afc269878d180aefbd95
MiRitter87 <michaelritter@gmx.net>
2024-07-29 07:55:11 +0200

    Remove info button from DashboardHealthStatus view.

--- webapp/controller/dashboard/DashboardHealthStatus.controller.js
+++ webapp/controller/dashboard/DashboardHealthStatus.controller.js
@@ -33,21 +33,7 @@ sap.ui.define([
 			
 			this.resetUIElements();
     	},
-    	
-    	
-    	/**
-    	 * Handles the button press event of the information button.
-    	 */
-    	onInfoButtonPressed : function() {
-			var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
-			var mOptions = new Object();
-			var sTitle = oResourceBundle.getText("dashboardHealthStatus.info.title");
-			var sDescription = oResourceBundle.getText("dashboardHealthStatus.info.description");
-			
-			mOptions.title = sTitle;
-			MessageBox.information(sDescription, mOptions);
-		},
-    	
+    	   	
     	
     	/**
     	 * Handles the button press event of the refresh health check button.
