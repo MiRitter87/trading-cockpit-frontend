e2a16de26788f31f2d5a88b56549895980ab1ff4
MiRitter87 <michaelritter@gmx.net>
2024-11-28 09:34:43 +0100

    Add header with infos to ChartPriceVolumeTV view.

--- webapp/controller/chart/priceVolume/ChartPriceVolumeTV.controller.js
+++ webapp/controller/chart/priceVolume/ChartPriceVolumeTV.controller.js
@@ -1,9 +1,29 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox"
+], function (Controller, MessageBox) {
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.chart.priceVolume.ChartPriceVolumeTV", {
-
+		/**
+		 * Initializes the controller.
+		 */
+		onInit : function () {
+			
+		},
+		
+		
+		/**
+    	 * Handles the button press event of the chart information button.
+    	 */
+    	onChartInformationPressed : function() {
+			var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			var mOptions = new Object();
+			var sTitle = oResourceBundle.getText("chartPriceVolumeTV.info.title");
+			var sDescription = oResourceBundle.getText("chartPriceVolumeTV.info.description");
+			
+			mOptions.title = sTitle;
+			MessageBox.information(sDescription, mOptions);
+		},
 	});
 });
