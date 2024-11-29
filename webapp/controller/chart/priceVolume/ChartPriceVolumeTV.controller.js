c4e58232659f4a5a9c6b59e370007037cd0cfe6b
MiRitter87 <michaelritter@gmx.net>
2024-11-29 09:38:27 +0100

    Query quotations for TradingView chart.

--- webapp/controller/chart/priceVolume/ChartPriceVolumeTV.controller.js
+++ webapp/controller/chart/priceVolume/ChartPriceVolumeTV.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+	"../../MainController",
 	"../../scan/ScanController",
 	"../../Constants",
 	"sap/ui/model/json/JSONModel",
 	"sap/m/MessageBox",
 	"sap/m/MessageToast",
 	"./lightweight-charts.standalone.production"
-], function (Controller, ScanController, Constants, JSONModel, MessageBox, MessageToast) {
+], function (Controller, MainController, ScanController, Constants, JSONModel, MessageBox, MessageToast) {
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.chart.priceVolume.ChartPriceVolumeTV", {
@@ -24,7 +25,7 @@ sap.ui.define([
 		 */
 		_onRouteMatched: function () {
 			//Query only instruments that have quotations referenced. Otherwise no chart could be created.
-			ScanController.queryQuotationsByWebService(this.queryQuotationsCallback, this, false, Constants.SCAN_TEMPLATE.ALL);
+			ScanController.queryQuotationsByWebService(this.queryAllQuotationsCallback, this, false, Constants.SCAN_TEMPLATE.ALL);
 		},
 		
 		
@@ -54,6 +55,82 @@ sap.ui.define([
     	 * Handles the button press event of the refresh chart button.
     	 */
     	onRefreshPressed : function() {
+			var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			var oInstrumentComboBox = this.getView().byId("instrumentComboBox");
+			var sSelectedInstrumentId = oInstrumentComboBox.getSelectedKey();
+			
+			if (sSelectedInstrumentId === "") {
+				MessageBox.error(oResourceBundle.getText("chartPriceVolumeTV.noInstrumentSelected"));
+				return;
+			}
+			
+			this.queryQuotationsOfInstrument(this.queryInstrumentQuotationsCallback, this, false, sSelectedInstrumentId);
+		},
+		
+		
+		/**
+		 * Callback function of the queryQuotationsByWebService RESTful WebService call in the ScanController.
+		 */
+		queryAllQuotationsCallback : function(oReturnData, oCallingController) {
+			var oModel = new JSONModel();
+			
+			if (oReturnData.data !== null) {
+				oModel.setSizeLimit(300);
+				oModel.setData(oReturnData.data);
+			}
+			
+			if (oReturnData.data === null && oReturnData.message !== null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}
+			
+			oCallingController.getView().setModel(oModel, "quotations");
+		},
+		
+		
+		/**
+		 * Callback function of the queryQuotationsOfInstrument RESTful WebService call.
+		 */
+		queryInstrumentQuotationsCallback : function(oReturnData, oCallingController) {
+			var oModel = new JSONModel();
+			
+			if (oReturnData.data !== null) {
+				oModel.setData(oReturnData.data);		
+			}
+			
+			if (oReturnData.data === null && oReturnData.message !== null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}                                                               
+			
+			oCallingController.getView().setModel(oModel, "quotationsForChart");
+			
+			oCallingController.openChart();
+		},
+		
+		
+		/**
+		 * Queries the quotation WebService for quotations of an Instrument with the given ID.
+		 */
+		queryQuotationsOfInstrument : function(callbackFunction, oCallingController, bShowSuccessMessage, sInstrumentId) {
+			var sServerAddress = MainController.getServerAddress();
+			var sWebServiceBaseUrl = oCallingController.getOwnerComponent().getModel("webServiceBaseUrls").getProperty("/quotation");
+			var sQueryUrl = sServerAddress + sWebServiceBaseUrl + "/" + sInstrumentId;
+			
+			jQuery.ajax({
+				type : "GET", 
+				contentType : "application/json", 
+				url : sQueryUrl, 
+				dataType : "json", 
+				success : function(data) {
+					callbackFunction(data, oCallingController, bShowSuccessMessage);
+				}
+			});  
+		},
+		
+		
+		/**
+		 * Handles initialization of the TradingView lightweight-charts component.
+		 */
+		openChart : function () {
 			var sDivId = "chartContainer";
 			
 			//Remove previously created chart for subsequent chart creations
@@ -89,25 +166,6 @@ sap.ui.define([
                 { time: '2023-01-04', value: 105 },
                 { time: '2023-01-05', value: 98 },
             ]);
-		},
-		
-		
-		/**
-		 * Callback function of the queryQuotationsByWebService RESTful WebService call in the ScanController.
-		 */
-		queryQuotationsCallback : function(oReturnData, oCallingController) {
-			var oModel = new JSONModel();
-			
-			if (oReturnData.data !== null) {
-				oModel.setSizeLimit(300);
-				oModel.setData(oReturnData.data);
-			}
-			
-			if (oReturnData.data === null && oReturnData.message !== null)  {
-				MessageToast.show(oReturnData.message[0].text);
-			}
-			
-			oCallingController.getView().setModel(oModel, "quotations");
 		}
 	});
 });
