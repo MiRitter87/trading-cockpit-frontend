298b87f067a0957cdf6f32386d00236445c6abb1
MiRitter87 <michaelritter@gmx.net>
2025-03-11 08:18:16 +0100

    Add general information button to TradingView health check view.

--- webapp/controller/chart/priceVolume/ChartHealthCheckTV.controller.js
+++ webapp/controller/chart/priceVolume/ChartHealthCheckTV.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox"
+], function (Controller, MessageBox) {
 	"use strict";
 
 	return Controller.extend("trading-cockpit-frontend.controller.chart.priceVolume.ChartHealthCheckTV", {
@@ -9,6 +10,20 @@ sap.ui.define([
 		 */
 		onInit : function () {
 
-		}
+		},
+		
+		
+		/**
+    	 * Handles the button press event of the chart information button.
+    	 */
+    	onChartInformationPressed : function() {
+			var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			var mOptions = new Object();
+			var sTitle = oResourceBundle.getText("chartHealthCheckTV.info.title");
+			var sDescription = oResourceBundle.getText("chartHealthCheckTV.info.description");
+			
+			mOptions.title = sTitle;
+			MessageBox.information(sDescription, mOptions);
+		},
 	});
 });
